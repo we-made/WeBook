236dd2bba7f232addb290db6dcf47917765276c0
magnuhar <magnus@r2it.no>
2021-10-12 12:31:23 +0200

    Register models with admin.py

--- webook/arrangement/admin.py
+++ webook/arrangement/admin.py
@@ -1,2 +1,23 @@
 from django.contrib import admin
+from .models import (Arrangement, Location, Room, Person, Event, Organization, OrganizationType, Article, Audience,
+                     BusinessHour, TimelineEvent, ServiceProvider, ServiceType, Calendar, Note, ConfirmationReceipt,)
 
+
+admin.site.register([
+    Arrangement,
+    Location,
+    Room,
+    Person,
+    Event,
+    Organization,
+    OrganizationType,
+    Article,
+    Audience,
+    BusinessHour,
+    TimelineEvent,
+    ServiceProvider,
+    ServiceType,
+    Calendar,
+    Note,
+    ConfirmationReceipt
+])
