78d77c092d0b93935c61ed6297f803d31e67a869
DESKTOP-T8VR2O2\Magnus Harlem <magnus@r2it.no>
2021-11-03 14:59:49 +0100

    add fullcalendar_license_key to context_processors to make it available

--- webook/utils/context_processors.py
+++ webook/utils/context_processors.py
@@ -3,4 +3,5 @@ from django.conf import settings
 
 def settings_context(_request):
     # Put global template variables here.
-    return {"DEBUG": settings.DEBUG, "APP_TITLE": settings.APP_TITLE, "APP_LOGO": settings.APP_LOGO }  # explicit
+    return {"DEBUG": settings.DEBUG, "APP_TITLE": settings.APP_TITLE, "APP_LOGO": settings.APP_LOGO,
+            "FULLCALENDAR_LICENSE_KEY": settings.FULLCALENDAR_LICENSE_KEY }  # explicit
