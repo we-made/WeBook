2f450ee6dcf3ba24d91da8f0ecbfc450a23d48fa
DESKTOP-T8VR2O2\Magnus Harlem <magnus@r2it.no>
2021-11-17 08:34:00 +0100

    add assets server url to settings context

--- webook/utils/context_processors.py
+++ webook/utils/context_processors.py
@@ -3,4 +3,4 @@ from django.conf import settings
 
 def settings_context(_request):
     # Put global template variables here.
-    return {"DEBUG": settings.DEBUG, "APP_TITLE": settings.APP_TITLE, "APP_LOGO": settings.APP_LOGO }  # explicit
+    return {"DEBUG": settings.DEBUG, "APP_TITLE": settings.APP_TITLE, "APP_LOGO": settings.APP_LOGO, "ASSET_SERVER_URL": settings.ASSET_SERVER_URL }  # explicit
