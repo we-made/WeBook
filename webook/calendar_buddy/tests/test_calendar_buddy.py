109feb60c914fcaef70c0809c862027c287db300
Magnus <magnus@roslyn.no>
2021-11-18 11:33:22 +0100

    finish tests of calendar_buddy.py

--- webook/calendar_buddy/tests/test_calendar_buddy.py
+++ webook/calendar_buddy/tests/test_calendar_buddy.py
@@ -3,7 +3,7 @@ from webook.calendar_buddy.base import CalendarContext
 from webook.calendar_buddy import calendar_buddy
 from webook.calendar_buddy.base import BaseCalendarContext, UIConfig
 from webook.calendar_buddy import factory_manager
-
+from webook.calendar_buddy.tests import test_factory_manager
 
 def simple_hook (ctx):
     print("Hook runs")
@@ -20,4 +20,29 @@ class TestCalendarBuddy:
         calendar_buddy.register_hook(simple_hook, context_type=CalendarContext.FULLCALENDAR)
         assert len(factory_manager._get_hooks_for_context_type(CalendarContext.FULLCALENDAR)) == 1
 
+    def test_register_defaults_for_event(self):
+        factory_manager.flush_state()
+        test_factory_manager.register_testing_calendarcontext_factory()
+        defaults = dict()
+        defaults["something"] = "something something"
+        calendar_buddy.register_defaults_for_events(
+            defaults=defaults,
+            context_type=CalendarContext.FULLCALENDAR
+        )
+        ctx_factory = factory_manager._CONTEXT_FACTORIES[1]
+        assert ctx_factory is not None and ctx_factory.event_standard is not None
+        assert "something" in ctx_factory.event_standard
+
+    def test_register_defaults_for_resource(self):
+        factory_manager.flush_state()
+        test_factory_manager.register_testing_calendarcontext_factory()
+        defaults = dict()
+        defaults["something"] = "something something"
+        calendar_buddy.register_defaults_for_resources(
+            defaults=defaults,
+            context_type=CalendarContext.FULLCALENDAR
+        )
+        ctx_factory = factory_manager._CONTEXT_FACTORIES[1]
+        assert ctx_factory is not None and ctx_factory.event_standard is not None
+        assert "something" in ctx_factory.resource_standard
     
