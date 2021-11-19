f9a287ab030f2009d220046dd98f03440bfea4c5
Magnus <magnus@roslyn.no>
2021-11-19 07:29:53 +0100

    add tests for fullcalendar context factory

--- webook/calendar_buddy/contexts/fullcalendar/tests/test_factory.py
+++ webook/calendar_buddy/contexts/fullcalendar/tests/test_factory.py
@@ -1,3 +1,4 @@
+from webook.calendar_buddy.contexts.fullcalendar.standard_library import resource_standard
 import pytest
 
 from webook.calendar_buddy.contexts.fullcalendar import factory
@@ -7,3 +8,10 @@ def test__repr__():
     result = test_factory.__repr__()
     assert result is not None  and type(result) is str and len(result) is not 0
     
+def test_fabricate():
+    test_factory = factory.FullCalendarFactory()
+    fabricated_context = test_factory.fabricate()
+
+    assert fabricated_context is not None
+    assert fabricated_context.ui_config is not None
+    assert resource_standard is not None
