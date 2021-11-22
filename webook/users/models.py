859c0e764702664d88483689b5e521d74cd73ae3
DESKTOP-T8VR2O2\Magnus Harlem <magnus@r2it.no>
2021-11-22 08:03:45 +0100

    add/fix profile_picture on User model

--- webook/users/models.py
+++ webook/users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+import media_pathing
 
 
 class User(AbstractUser):
@@ -10,6 +11,11 @@ class User(AbstractUser):
     # Around the Globe.
     name = models.CharField(
         _("Name of User"), blank=True, max_length=255
+    )
+
+    profile_picture = models.FileField(
+        verbose_name="Profile Picture",
+        upload_to=media_pathing.profile_picture_path
     )
 
     def get_absolute_url(self):
