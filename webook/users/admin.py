3f56ef1830a7fb69f671ba6e38b2eb963e335bf3
DESKTOP-T8VR2O2\Magnus Harlem <magnus@r2it.no>
2021-11-23 12:55:25 +0100

    Remove name from admin user model registration

--- webook/users/admin.py
+++ webook/users/admin.py
@@ -16,7 +16,7 @@ class CustomUserAdmin(auth_admin.UserAdmin):
     list_display = ('email', 'is_staff', 'is_active',)
     list_filter = ('email', 'is_staff', 'is_active',)
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'name')}),
+        (None, {'fields': ('email', 'password')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
