486111a3d716011135d58c6ac22e0095bb63b0f8
DESKTOP-T8VR2O2\Magnus Harlem <magnus@r2it.no>
2021-11-23 08:46:45 +0100

    use slug in views, not username

--- webook/users/views.py
+++ webook/users/views.py
@@ -14,8 +14,8 @@ class UserDetailView(LoginRequiredMixin, DetailView):
     model = User
     # These Next Two Lines Tell the View to Index
     #   Lookups by Username
-    slug_field = "username"
-    slug_url_kwarg = "username"
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
 
 
 user_detail_view = UserDetailView.as_view()
@@ -35,14 +35,14 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
     def get_success_url(self):
         return reverse(
             "users:detail",
-            kwargs={'username': self.request.user.username},
+            kwargs={'slug': self.request.user.slug},
         )
 
     def get_object(self):
         # Only Get the User Record for the
         #   User Making the Request
         return User.objects.get(
-            username=self.request.user.username
+            slug=self.request.user.slug
         )
 
 
@@ -55,7 +55,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
     def get_redirect_url(self):
         return reverse(
             "users:detail",
-            kwargs={"username": self.request.user.username},
+            kwargs={"username": self.request.user.slug},
         )
 
 
