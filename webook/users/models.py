030269b83b23f2b2c9535fb4bbe32f26115be8ce
DESKTOP-T8VR2O2\Magnus Harlem <magnus@r2it.no>
2021-11-23 08:39:51 +0100

    add custom usermanager, and slug to user model

--- webook/users/models.py
+++ webook/users/models.py
@@ -1,18 +1,65 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from autoslug import AutoSlugField
+
+
+class CustomUserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     # First Name and Last Name Do Not Cover Name Patterns
     # Around the Globe.
     name = models.CharField(
         _("Name of User"), blank=True, max_length=255
     )
+    slug = AutoSlugField(populate_from="name", blank=True)
 
-    def get_absolute_url(self):
-        return reverse(
-            "users:detail", kwargs={"username": self.username}
-        )
+    # def get_absolute_url(self):
+    #     return reverse(
+    #         "users:detail", kwargs={"username": self.username}
+    #     )
+
+    def __str__(self) -> str:
+        return self.email
