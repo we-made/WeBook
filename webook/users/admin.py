daaa4878538610bfe7b3ea44ab036483e260d942
DESKTOP-T8VR2O2\Magnus Harlem <magnus@r2it.no>
2021-11-23 08:56:06 +0100

    remove commented code

--- webook/users/admin.py
+++ webook/users/admin.py
@@ -10,17 +10,6 @@ from webook.users.forms import (
 User = get_user_model()
 
 
-# @admin.register(User)
-# class UserAdmin(auth_admin.UserAdmin):
-
-#     form = UserChangeForm
-#     add_form = UserCreationForm
-#     fieldsets = (
-#         ("User", {"fields": ("name",)}),
-#     ) + auth_admin.UserAdmin.fieldsets
-#     list_display = ["username", "name", "is_superuser"]
-#     search_fields = ["name"]
-
 @admin.register(User)
 class CustomUserAdmin(auth_admin.UserAdmin):
     model = User
