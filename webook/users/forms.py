4da6a2fbb5b069a6602561caad69e02224b83a95
DESKTOP-T8VR2O2\Magnus Harlem <magnus@r2it.no>
2021-11-23 08:38:24 +0100

    rewrite UserCreationForm

--- webook/users/forms.py
+++ webook/users/forms.py
@@ -1,36 +1,26 @@
 from django.contrib.auth import get_user_model, forms
 from django.core.exceptions import ValidationError
+from django.db.models import fields
 from django.utils.translation import gettext_lazy as _
+from django import forms as dj_forms
+
+from allauth.account.forms import SignupForm
 
 User = get_user_model()
 
 
 class UserChangeForm(forms.UserChangeForm):
     class Meta(forms.UserChangeForm.Meta):
+        fields = ()
+        field_classes = {}
         model = User
 
+class UserCreationForm(SignupForm):
 
-class UserCreationForm(forms.UserCreationForm):
-
-    error_message = forms.UserCreationForm.error_messages.update(
-        {
-            "duplicate_username": _(
-                "This username has already been taken."
-            )
-        }
-    )
-
-    class Meta(forms.UserCreationForm.Meta):
-        model = User
-
-    def clean_username(self):
-        username = self.cleaned_data["username"]
-
-        try:
-            User.objects.get(username=username)
-        except User.DoesNotExist:
-            return username
-
-        raise ValidationError(
-            self.error_messages["duplicate_username"]
-        )
+    name = dj_forms.CharField(max_length=512, label="Navn")
+    
+    def save (self, request):
+        user = super(UserCreationForm, self).save(request)
+        user.name = self.cleaned_data['name']
+        user.save()
+        return user
