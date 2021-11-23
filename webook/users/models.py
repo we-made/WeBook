6bf4c863d51f2eef9e00fa107a2418aed54e0e7e
DESKTOP-T8VR2O2\Magnus Harlem <magnus@r2it.no>
2021-11-23 09:30:46 +0100

    add required person fk to User

--- webook/users/models.py
+++ webook/users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from WeBook.webook.arrangement.models import Person
 from autoslug import AutoSlugField
 
 
@@ -48,6 +49,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
+
+    person = models.ForeignKey(Person, blank=False)
 
     slug = AutoSlugField(populate_from="name", blank=True)
 
