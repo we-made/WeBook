4e1f504d2868f5647f3d2d654b38c1a834659205
DESKTOP-T8VR2O2\Magnus Harlem <magnus@r2it.no>
2021-11-24 09:47:29 +0100

    fix import

--- webook/arrangement/models.py
+++ webook/arrangement/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
-from django_extensions.db.fields import AutoSlugField
+from autoslug import AutoSlugField
 
 
 class Audience(TimeStampedModel):
