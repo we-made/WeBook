9d9dc9504fc7a027ab6079f3b2d541f73a41fd7a
DESKTOP-T8VR2O2\Magnus Harlem <magnus@r2it.no>
2021-11-24 09:13:56 +0100

    rewrite and add new tests

--- webook/users/tests/test_forms.py
+++ webook/users/tests/test_forms.py
@@ -1,8 +1,11 @@
 import pytest
 
+from webook.users.models import User
 from webook.users.forms import UserCreationForm
 from webook.users.tests.factories import UserFactory
+from django.test import RequestFactory
 
 pytestmark = pytest.mark.django_db
+
 
 
