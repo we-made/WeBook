72fa6a4943c054912ea9e460480a593f31a6252e
DESKTOP-T8VR2O2\Magnus Harlem <magnus@r2it.no>
2021-11-29 13:49:37 +0100

    write docstring for MediativeEvent

--- webook/calendar_buddy/mediative/mediative_event.py
+++ webook/calendar_buddy/mediative/mediative_event.py
@@ -3,6 +3,14 @@ from datetime import datetime
 import json
 
 class MediativeEvent:
+    """
+        MediativeEvent is the bare minimum an event type for a context must implement. 
+        It serves as the "common tongue" that CalendarBuddy understands, and can work with.
+        The kwargs attribute allows you to specify extra attributes, and through the use of translators
+        in your context implementation you should be able to build a fully fledged event that matches
+        the contexts domain.
+    """
+
     def __init__(self, id: str, title: str, start: datetime, end: datetime, **kwargs):
         self.id = id
         self.title = title
