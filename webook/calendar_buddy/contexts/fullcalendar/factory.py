cbb09b2aaac96a9247daf2a75c3c4980113bcfb7
DESKTOP-T8VR2O2\Magnus Harlem <magnus@r2it.no>
2021-11-29 14:00:18 +0100

    write docstrings for factory.py in fc context

--- webook/calendar_buddy/contexts/fullcalendar/factory.py
+++ webook/calendar_buddy/contexts/fullcalendar/factory.py
@@ -6,7 +6,23 @@ import datetime
 
 
 class FullCalendarFactory(base.BaseCalendarContextFactory):
-    def __init__(self, standard_ui_config: base.UIConfig = None, event_standard: dict() = None, resource_standard: dict() = None):
+    """
+        A factory which can fabricate FullCalendarContexts
+    """
+
+    def __init__(self, standard_ui_config: base.UIConfig = None, event_standard: dict() = None, resource_standard: dict() = None) -> None:
+        """
+            Initialize the FullCalendarFactory
+
+            :param standard_ui_config: The standard base UI config that this factory is to adhere to when fabricating in the future. May be None.
+            :type standard_ui_config: UIConfig
+
+            :param event_standard: The standard event defaults that this factory is to adhere to when fabricating. May be None.
+            :type event_standard: dict
+
+            :param resource_standard: The standard resource defaults that this factory is to adhere to when fabricating. May be None.
+            :type resource_standard: dict
+        """
         super().__init__()
         self.standard_ui_config = base.UIConfig().overwrite(standard_ui_config)
         self.event_standard = self._get_standard_event_default(specified_defaults = event_standard)
@@ -46,6 +62,18 @@ class FullCalendarFactory(base.BaseCalendarContextFactory):
 
 
     def fabricate(self, custom_event_standard: list() = None, custom_resource_standard: list() = None) -> FullCalendarContext:
+        """
+            Fabricate a new FullCalendarContext
+
+            :param custom_event_standard: A event standard to use for just this fabrication
+            :type custom_event_standard: list
+
+            :param custom_resource_standard: A resource standard to use for just this fabrication
+            :type custom_resource_standard: list
+
+            :return: Returns a newly fabricated FullCalendarContext, composed and molded based on the standards and hooks supplied
+            :rtype: FullCalendarContext
+        """
         if (custom_event_standard is None):
             custom_event_standard = self.event_standard
         if (custom_resource_standard is None):
