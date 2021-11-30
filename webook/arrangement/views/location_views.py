3276a6f315f9c8125b622e85b0950677c48f8d4a
Magnus <magnus@roslyn.no>
2021-11-30 11:04:17 +0100

    set custom template_name on views, to keep templates directory structure clean

--- webook/arrangement/views/location_views.py
+++ webook/arrangement/views/location_views.py
@@ -12,6 +12,7 @@ from webook.arrangement.models import Location
 
 class LocationListView(LoginRequiredMixin, ListView):
     queryset = Location.objects.all()
+    template_name = "arrangement/location/location_list.html" 
 
 location_list_view = LocationListView.as_view()
 
@@ -21,6 +22,8 @@ class LocationDetailView(LoginRequiredMixin, DetailView):
     slug_field = "slug"
     slug_url_kwarg = "slug"
 
+    template_name = "arrangement/location/location_detail.html" 
+
 location_detail_view = LocationDetailView.as_view()
 
 
@@ -28,7 +31,9 @@ class LocationUpdateView(LoginRequiredMixin, UpdateView):
     fields = [
         "name"
     ]
-    
+
+    template_name = "arrangement/location/location_form.html" 
+
     model = Location
 
 location_update_view = LocationUpdateView.as_view()
@@ -38,6 +43,8 @@ class LocationCreateView(LoginRequiredMixin, CreateView):
     fields = [
         "name"
     ]
+
+    template_name = "arrangement/location/location_form.html" 
 
     model = Location
 
