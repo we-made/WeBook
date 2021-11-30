ec4045e059ea053b44615e8ae1495a003648d0bd
Magnus <magnus@roslyn.no>
2021-11-30 10:52:43 +0100

    import arrangement views

--- webook/arrangement/views/__init__.py
+++ webook/arrangement/views/__init__.py
@@ -4,3 +4,10 @@
 #from .arrangement_views import (
 #   arrangement_create_view
 #)
+
+from .location_views import (
+    location_detail_view,
+    location_update_view,
+    location_list_view
+)
+
