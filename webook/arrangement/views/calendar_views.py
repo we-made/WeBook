3a2e7a27bec34dc0a42d1ab8b7fe444bf7e5c29d
Magnus <magnus@roslyn.no>
2021-11-30 22:09:58 +0100

    additional changes

---
+++ webook/arrangement/views/calendar_views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import (
+    DetailView,
+    RedirectView,
+    UpdateView,
+    ListView,
+    CreateView,
+)
+from webook.arrangement.models import Event, Person, Room
