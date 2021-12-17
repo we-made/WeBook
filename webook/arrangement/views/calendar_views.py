e00f81237398c8b9262dc0e6e22b3e60cec850c0
Magnus <magnus@roslyn.no>
2021-12-17 11:47:09 +0100

    rewrite calendar_views meta stuff

--- webook/arrangement/views/calendar_views.py
+++ webook/arrangement/views/calendar_views.py
@@ -11,19 +11,24 @@ from django.views.generic import (
 )
 from webook.arrangement.models import Event, Location, Person, Room
 from webook.arrangement.views.custom_views.crumb_view import CrumbMixin
+from webook.utils.meta_utils import SectionManifest, ViewMeta, SectionCrudlPathMap
 
 
-section_manifest = {
-    "SECTION_TITLE": _("Calendars"),
-    "SECTION_ICON": "fas fa-calendar",
-    "SECTION_CRUMB_URL": lambda: reverse("arrangement:arrangement_calendar")
-}
+section_manifest = SectionManifest(
+    section_title=_("Calendars"),
+    section_icon= "fas fa-calendar",
+    section_crumb_url=lambda: reverse("arrangement:arrangement_calendar")
+)
+
 
 class CalendarSamplesOverview (LoginRequiredMixin, CrumbMixin, TemplateView):
     template_name = "arrangement/calendar/calendars_list.html"
     section = section_manifest
-    current_crumb_title = _("Calendar Samples")
-    section_subtitle = _("Calendar Samples")
+
+    view_meta=ViewMeta(
+        subtitle=_("Calendar Samples"),
+        current_crumb_title=_("Calendar Samples")
+    )
 
 calendar_samples_overview = CalendarSamplesOverview.as_view()
 
@@ -31,8 +36,11 @@ calendar_samples_overview = CalendarSamplesOverview.as_view()
 class ArrangementCalendarView (LoginRequiredMixin, CrumbMixin, TemplateView):
     template_name = "arrangement/calendar/arrangement_calendar.html"
     section = section_manifest
-    current_crumb_title = _("Arrangement Calendar")
-    section_subtitle = _("Arrangement Calendar")
+
+    view_meta = ViewMeta(
+        subtitle=_("Arrangement Calendar"),
+        current_crumb_title=_("Arrangement Calendar")
+    )
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
