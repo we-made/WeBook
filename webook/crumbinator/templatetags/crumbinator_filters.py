49748d5b290296af20728c88c87c674a636b9a81
Magnus <magnus@roslyn.no>
2021-12-17 15:53:41 +0100

    add comment about design constraint

--- webook/crumbinator/templatetags/crumbinator_filters.py
+++ webook/crumbinator/templatetags/crumbinator_filters.py
@@ -9,6 +9,8 @@ register = template.Library()
 @register.filter(name="crumbinator")
 def crumbinator(tree: Node):
     unpacked_tree = list()
+    # this is temporary :) -- This constrains the tree to be linear, and we cant have dropdown options
+    # This should be fixed later on
     for pre, fill, node in RenderTree(tree):
         unpacked_tree.append(node)
     
