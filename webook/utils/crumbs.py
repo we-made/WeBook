28b7d757f1bf7e449022d3ab87d0db66af6c5597
Magnus <magnus@roslyn.no>
2021-12-22 10:23:22 +0100

    add type hint to get_root_crumb user_slug param

--- webook/utils/crumbs.py
+++ webook/utils/crumbs.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 
 
-def get_root_crumb(user_slug):
+def get_root_crumb(user_slug: str):
     return CrumbNode(
         title=_("Home"),
         url=reverse("users:detail", kwargs={"slug": user_slug}),
