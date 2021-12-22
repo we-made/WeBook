81cb6afb4068166816c3f1d20aaecaa0e353b715
Magnus <magnus@roslyn.no>
2021-12-22 10:52:15 +0100

    write unit tests for crumb_node

--- webook/crumbinator/tests.py
+++ webook/crumbinator/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+import pytest
+from webook.crumbinator.crumb_node import CrumbNode
 
-# Create your tests here.
+
+def test_crumb_node_icon_html():
+    node = CrumbNode(
+        title="Something",
+        url="#",
+        icon_class="fas fa-question-circle",
+    )
+    
+    assert node.icon_html == "<i class='fas fa-question-circle'></i> "
+
+
+def test_crumb_node_joined_html_classes():
+    node = CrumbNode(
+        title="Something",
+        url="#",
+        html_classes=["class1", "class2", "class3", "class4"]
+    )
+
+    assert node.joined_html_classes == "class1 class2 class3 class4"
+
+def test_crumb_node_as_html():
+    node = CrumbNode(
+        title="Something",
+        url="#",
+        html_classes=["class1", "class2", "class3"],
+        icon_class="fas fa-question-circle"
+    )
+
+    assert node.as_html == "<a href='#' class='class1 class2 class3'><i class='fas fa-question-circle'></i> Something</a>"
