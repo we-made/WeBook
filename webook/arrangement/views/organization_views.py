041385ef2617ab57c125897d58c154d7a0b28158
Magnus <magnus@roslyn.no>
2021-12-22 10:07:24 +0100

    fix import of sectioncrudlpathmap in organization_views

--- webook/arrangement/views/organization_views.py
+++ webook/arrangement/views/organization_views.py
@@ -12,7 +12,7 @@ from django.views.generic.base import View
 from django.views.generic.edit import DeleteView
 from webook.arrangement.models import Organization
 from webook.arrangement.views.custom_views.crumb_view import CrumbMixin
-from webook.utils.crudl_utils.path_maps import SectionCrudlPathMap
+from webook.utils.meta_utils.section_manifest import SectionCrudlPathMap
 from webook.utils.crudl_utils.view_mixins import GenericListTemplateMixin
 from webook.utils.meta_utils import SectionManifest, ViewMeta, SectionCrudlPathMap
 
