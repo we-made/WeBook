65b7f4ff7606e00bce9379d6262c7d8c9462cd58
Magnus <magnus@roslyn.no>
2021-12-27 11:20:51 +0100

    add arrangement calendar view

--- webook/arrangement/views/calendar_views.py
+++ webook/arrangement/views/calendar_views.py
@@ -9,6 +9,7 @@ from django.views.generic import (
     CreateView,
     TemplateView
 )
+from webook.arrangement.models import Person, Location
 from django.views.generic.edit import DeleteView
 from webook.utils.meta_utils.meta_mixin import MetaMixin
 from webook.utils.meta_utils.section_manifest import SectionCrudlPathMap
@@ -28,3 +29,27 @@ class CalendarSectionManifestMixin:
     def __init__(self) -> None:
         super().__init__()
         self.section = get_section_manifest()
+
+
+class CalendarSamplesOverview (LoginRequiredMixin, CalendarSectionManifestMixin, MetaMixin, TemplateView):
+    template_name = "arrangement/calendar/calendars_list.html"
+    view_meta=ViewMeta(
+        subtitle=_("Calendar Samples"),
+        current_crumb_title=_("Calendar Samples")
+    )
+
+calendar_samples_overview = CalendarSamplesOverview.as_view()
+
+
+class ArrangementCalendarView (LoginRequiredMixin, CalendarSectionManifestMixin, MetaMixin, TemplateView):
+    template_name = "arrangement/calendar/arrangement_calendar.html"
+    view_meta = ViewMeta(
+        subtitle=_("Arrangement Calendar"),
+        current_crumb_title=_("Arrangement Calendar")
+    )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["locations"] = Location.objects.all()
+        context["people"] = Person.objects.all()
+        return context
