15d1c09bca67e84bb40e4ea71786e8668d114b22
Magnus <magnus@roslyn.no>
2021-12-27 12:00:59 +0100

    fix imports in insight_views

--- webook/arrangement/views/insight_views.py
+++ webook/arrangement/views/insight_views.py
@@ -2,18 +2,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import (
-    DetailView,
-    RedirectView,
-    UpdateView,
-    ListView,
-    CreateView,
     TemplateView
 )
-from django.views.generic.edit import DeleteView
-from webook.arrangement.models import Arrangement
+from webook.arrangement.models import Location
+import json
 from webook.utils.meta.meta_model_mixins import MetaMixin
-from webook.utils.meta.meta_view_mixins import GenericListTemplateMixin
-from webook.utils.meta.meta_types import SectionManifest, ViewMeta, SectionCrudlPathMap
+from webook.utils.meta.meta_types import SectionManifest, ViewMeta
 
 
 def get_section_manifest():
