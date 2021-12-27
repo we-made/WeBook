f3422541256b26a0966e20b3d652eb0d86e898f3
Magnus <magnus@roslyn.no>
2021-12-27 13:18:59 +0100

    import servicetype views in __init__.py

--- webook/arrangement/views/__init__.py
+++ webook/arrangement/views/__init__.py
@@ -4,3 +4,11 @@
 #from .arrangement_views import (
 #   arrangement_create_view
 #)
+
+from .servicetype_views import (
+    service_type_detail_view,
+    service_type_list_view,
+    service_type_create_view,
+    service_type_update_view,
+    service_type_delete_view,
+)
