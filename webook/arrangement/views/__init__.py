a14d0e9423cab8e5474755c3eed93d716bb45d71
Magnus <magnus@roslyn.no>
2021-12-27 12:38:11 +0100

    import organizationtype views to __init__.,py

--- webook/arrangement/views/__init__.py
+++ webook/arrangement/views/__init__.py
@@ -4,3 +4,11 @@
 #from .arrangement_views import (
 #   arrangement_create_view
 #)
+
+from .organizationtype_views import (
+    organization_type_detail_view,
+    organization_type_list_view,
+    organization_type_create_view,
+    organization_type_update_view,
+    organization_type_delete_view,
+)
