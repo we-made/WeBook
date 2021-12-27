ad97dd41db6558d818b29c411863d2ac48f991fd
Magnus <magnus@roslyn.no>
2021-12-27 11:18:23 +0100

    set up basics on calendar_views.py

---
+++ webook/arrangement/views/calendar_views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import (
+    DetailView,
+    RedirectView,
+    UpdateView,
+    ListView,
+    CreateView,
+    TemplateView
+)
+from django.views.generic.edit import DeleteView
+from webook.utils.meta_utils.meta_mixin import MetaMixin
+from webook.utils.meta_utils.section_manifest import SectionCrudlPathMap
+from webook.utils.crudl_utils.view_mixins import GenericListTemplateMixin
+from webook.utils.meta_utils import SectionManifest, ViewMeta, SectionCrudlPathMap
+
+
+def get_section_manifest():
+    return SectionManifest(
+        section_title=_("Calendars"),
+        section_icon= "fas fa-calendar",
+        section_crumb_url=reverse("arrangement:arrangement_calendar")
+    )
