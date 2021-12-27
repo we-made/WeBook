c111bf2aeae33a8879b2b3605f533e5d85095f38
Magnus <magnus@roslyn.no>
2021-12-27 12:47:48 +0100

    setup basics for person_views

---
+++ webook/arrangement/views/person_views.py
@@ -0,0 +1,36 @@
+
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import (
+    DetailView,
+    RedirectView,
+    UpdateView,
+    ListView,
+    CreateView,
+    TemplateView
+)
+from webook.arrangement.models import Person
+from django.views.generic.edit import DeleteView
+from webook.utils.meta.meta_view_mixins import MetaMixin, GenericListTemplateMixin
+from webook.utils.meta.meta_types import SectionManifest, ViewMeta, SectionCrudlPathMap
+
+
+def get_section_manifest():
+    return SectionManifest(
+        section_title=_("People"),
+        section_icon="fas fa-users",
+        section_crumb_url=reverse("arrangement:location_list"),
+        crudl_map=SectionCrudlPathMap(
+            detail_url="arrangement:person_detail",
+            create_url="arrangement:person_create",
+            edit_url="arrangement:person_edit",
+            delete_url="arrangement:person_delete",
+            list_url="arrangement:person_list",
+        )
+    )
+
+
+class PersonSectionManifestMixin:
+    def __init__(self) -> None:
+        super().__init__()
+        self.section = get_section_manifest()
