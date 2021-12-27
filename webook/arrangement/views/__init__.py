93320a963b1e963a5c19225d52a765651c5bb87e
Magnus <magnus@roslyn.no>
2021-12-27 11:07:46 +0100

    import audience view in __init__.py

--- webook/arrangement/views/__init__.py
+++ webook/arrangement/views/__init__.py
@@ -4,3 +4,11 @@
 #from .arrangement_views import (
 #   arrangement_create_view
 #)
+
+from .audience_views import (
+    audience_detail_view,
+    audience_list_view,
+    audience_create_view,
+    audience_update_view,
+    audience_delete_view,
+)
