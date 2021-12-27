1383bb5ff88661efe9a55bccaecc6d2ef6dd3080
Magnus <magnus@roslyn.no>
2021-12-27 12:54:45 +0100

    import person views to __init__,py

--- webook/arrangement/views/__init__.py
+++ webook/arrangement/views/__init__.py
@@ -4,3 +4,11 @@
 #from .arrangement_views import (
 #   arrangement_create_view
 #)
+
+from .person_views import (
+    person_detail_view,
+    person_list_view,
+    person_create_view,
+    person_update_view,
+    person_delete_view,
+)
