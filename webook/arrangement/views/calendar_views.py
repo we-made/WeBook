519c6eeee280c21e1629d2e07117dc21229c02fd
Magnus <magnus@roslyn.no>
2021-12-27 12:21:38 +0100

    fix calendar_views meta imports

--- webook/arrangement/views/calendar_views.py
+++ webook/arrangement/views/calendar_views.py
@@ -2,19 +2,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import (
-    DetailView,
-    RedirectView,
-    UpdateView,
-    ListView,
-    CreateView,
     TemplateView
 )
 from webook.arrangement.models import Person, Location
-from django.views.generic.edit import DeleteView
-from webook.utils.meta_utils.meta_mixin import MetaMixin
-from webook.utils.meta_utils.section_manifest import SectionCrudlPathMap
-from webook.utils.crudl_utils.view_mixins import GenericListTemplateMixin
-from webook.utils.meta_utils import SectionManifest, ViewMeta, SectionCrudlPathMap
+from webook.utils.meta.meta_view_mixins import MetaMixin
+from webook.utils.meta.meta_types import SectionManifest, ViewMeta, SectionCrudlPathMap
 
 
 def get_section_manifest():
