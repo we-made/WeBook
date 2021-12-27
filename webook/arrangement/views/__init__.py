7e9de0af7d6e0bfb88f88e1a7005f4288614f3cf
Magnus <magnus@roslyn.no>
2021-12-27 13:05:51 +0100

    import room_views to __init__.py

--- webook/arrangement/views/__init__.py
+++ webook/arrangement/views/__init__.py
@@ -4,3 +4,11 @@
 #from .arrangement_views import (
 #   arrangement_create_view
 #)
+
+from .room_views import (
+    room_detail_view,
+    room_list_view,
+    room_create_view,
+    room_update_view,
+    room_delete_view,
+)
