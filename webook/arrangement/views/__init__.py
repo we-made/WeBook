e83f24cfc2549ad4368ec4d68bfe58d4f82b30c5
Magnus <magnus@roslyn.no>
2021-12-27 12:13:28 +0100

    import location views to __init__.py

--- webook/arrangement/views/__init__.py
+++ webook/arrangement/views/__init__.py
@@ -4,3 +4,11 @@
 #from .arrangement_views import (
 #   arrangement_create_view
 #)
+
+from .location_views import (
+    location_detail_view,
+    location_list_view,
+    location_create_view,
+    location_update_view,
+    location_delete_view,
+)
