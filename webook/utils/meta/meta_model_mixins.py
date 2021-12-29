6a486173a9db9a473439dc2523d45b9aab24beae
Magnus <magnus@roslyn.no>
2021-12-29 14:24:59 +0100

    remove singular and plural attributes from ModelNamingMetaMixin

--- webook/utils/meta/meta_model_mixins.py
+++ webook/utils/meta/meta_model_mixins.py
@@ -6,8 +6,6 @@ class ModelNamingMetaMixin:
         Mixin to help with getting info of entity naming, of the instance, and plurality/singularity forms of entity name.
     """
     instance_name_attribute_name = "name"
-    entity_name_plural = "Entity"
-    entity_name_singular = "Entities"
 
     @property
     def resolved_name(self):
