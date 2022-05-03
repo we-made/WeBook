cfa7a5969f604e8b52f2b40454253af3a859623b
Magnus <magnus@wemade.no>
2022-05-03 12:57:00 +0200

    supply context instance when calling onRenderedCallback in dialogmanager

--- webook/static/modules/planner/dialog_manager/dialogManager.js
+++ webook/static/modules/planner/dialog_manager/dialogManager.js
@@ -28,7 +28,7 @@
             $('body')
                 .append(await this.htmlFabricator(context))
                 .ready( () => {
-                    this.onRenderedCallback(this);
+                    this.onRenderedCallback(this, context);
                     this._$getDialogEl().dialog( this.dialogOptions );    
                     this._$getDialogEl().dialog("widget").find('.ui-dialog-titlebar-close')
                         .html("<i class='fas fa-times text-danger' style='font-size: 24px'></i>");
