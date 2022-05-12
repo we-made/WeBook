488d4700e108ddc29b2a45722328348a259f1025
Eivind Teig <eivind.teig@gmail.com>
2022-05-12 10:29:22 +0200

    Update allowed hosts to prod.

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,10 @@ SECRET_KEY = env(
     default="!!!SET DJANGO_SECRET_KEY!!!",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS", "localhost", "0.0.0.0", "127.0.0.1", default=["webook.wemade.no"]
+)
+
 
 # CACHES
 # ------------------------------------------------------------------------------
