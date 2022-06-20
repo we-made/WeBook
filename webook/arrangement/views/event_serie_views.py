358f5390b08e60b5a966cf062a3928eba96d4e67
DESKTOP-T8VR2O2\Magnus Harlem <magnus@r2it.no>
2022-06-20 09:55:44 +0200

    remove view not to be implemented

--- webook/arrangement/views/event_serie_views.py
+++ webook/arrangement/views/event_serie_views.py
@@ -1,35 +1,29 @@
 from typing import Any
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core import serializers
+from django.db.models import Q
+from django.http import Http404, HttpRequest, HttpResponse, JsonResponse
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views import View
-from django.db.models import Q
-from django.core import serializers
-from django.views.generic import (
-    DetailView,
-    RedirectView,
-    UpdateView,
-    ListView,
-    CreateView,
-    TemplateView
-)
-from django.views.generic.edit import DeleteView
+from django.views.generic import CreateView, DetailView, ListView, RedirectView, TemplateView, UpdateView
+from django.views.generic.edit import DeleteView, FormView
 from requests import delete
+
+from webook.arrangement.forms.add_planner_form import AddPlannerForm
 from webook.arrangement.forms.delete_arrangement_file_form import DeleteArrangementFileForm
 from webook.arrangement.forms.planner.planner_create_arrangement_form import PlannerCreateArrangementModelForm
 from webook.arrangement.forms.promote_planner_to_main_form import PromotePlannerToMainForm
 from webook.arrangement.forms.remove_planner_form import RemovePlannerForm
-from webook.arrangement.forms.add_planner_form import AddPlannerForm
 from webook.arrangement.models import Arrangement, ArrangementFile, EventSerie, EventSerieFile, Person, PlanManifest
 from webook.arrangement.views.generic_views.archive_view import ArchiveView, JsonArchiveView
-from webook.arrangement.views.mixins.json_response_mixin import JSONResponseMixin
 from webook.arrangement.views.generic_views.search_view import SearchView
-from webook.utils.meta_utils.meta_mixin import MetaMixin
-from django.views.generic.edit import FormView
-from django.http import Http404, HttpRequest, HttpResponse, JsonResponse
-from webook.utils.meta_utils.section_manifest import SectionCrudlPathMap
+from webook.arrangement.views.mixins.json_response_mixin import JSONResponseMixin
 from webook.utils.crudl_utils.view_mixins import GenericListTemplateMixin
-from webook.utils.meta_utils import SectionManifest, ViewMeta, SectionCrudlPathMap
+from webook.utils.meta_utils import SectionCrudlPathMap, SectionManifest, ViewMeta
+from webook.utils.meta_utils.meta_mixin import MetaMixin
+from webook.utils.meta_utils.section_manifest import SectionCrudlPathMap
 from webook.utils.serie_calculator import calculate_serie
 
 
@@ -50,11 +44,6 @@ class DeleteEventSerie(LoginRequiredMixin, JsonArchiveView):
     pk_url_kwarg = "pk"
 
 delete_event_serie_view = DeleteEventSerie.as_view()
-
-
-class CalculateNonExistantSerie(LoginRequiredMixin, DetailView, JSONResponseMixin):
-    """ Calculate a serie or schedule which is not yet saved in the back-end with a manifest """
-    pass
 
 
 class CalculateEventSerieView(LoginRequiredMixin, DetailView, JSONResponseMixin):
