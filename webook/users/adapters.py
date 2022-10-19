1b9e20803b96fb1c55fb7cb2803b2d58b45e49ad
DESKTOP-T8VR2O2\Magnus Harlem <magnus@wemade.no>
2022-10-19 08:09:55 +0200

    set up sso provider and stuff

--- webook/users/adapters.py
+++ webook/users/adapters.py
@@ -1,24 +1,30 @@
 from typing import Any
 
 from allauth.account.adapter import DefaultAccountAdapter
-from allauth.socialaccount.adapter import (
-    DefaultSocialAccountAdapter,
-)
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.conf import settings
 from django.http import HttpRequest
 
 
 class AccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request: HttpRequest):
-        return getattr(
-            settings, "ACCOUNT_ALLOW_REGISTRATION", True
-        )
+        return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
-    def is_open_for_signup(
-        self, request: HttpRequest, sociallogin: Any
-    ):
-        return getattr(
-            settings, "ACCOUNT_ALLOW_REGISTRATION", True
-        )
+    def is_open_for_signup(self, request: HttpRequest, sociallogin: Any):
+        return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
+
+
+class MicrosoftPersonAccountAdapter(DefaultSocialAccountAdapter):
+    """Custom SocialAccountAdapter for integration with Azure Active Directory / Graph API through AllAuth"""
+    pass
+    # def pre_social_login(self, request, sociallogin):
+    #     """ Invoked after a user successsfully authenticates via the social provider. """
+    #     super().pre_social_login(request, sociallogin)
+
+
+    # def authentication_error(self, *args, **kwargs):
+    #     """ Invoked when there is an error in the AUTH cycle """
+    #     super().authentication_error(args, kwargs)
+
