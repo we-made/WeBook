d6809c4090baa224542ebc620272726809948253
DESKTOP-T8VR2O2\Magnus Harlem <magnus@wemade.no>
2022-11-08 08:23:57 +0100

    fix some issues with SSO, and connecting of social accounts

--- webook/users/adapters.py
+++ webook/users/adapters.py
@@ -28,27 +28,32 @@ class MicrosoftPersonAccountAdapter(DefaultSocialAccountAdapter):
     With custom logic requiring a Person item to exist for the Social Login to start with.
     """
 
+    def _triggerStandardErrorPage(self, reasoning_message: str):
+        context = settings_context(_request=None)
+        if getattr(settings, "DISPLAY_SSO_ERROR_REASONING", False) is True:
+            context["reasoning_message"] = reasoning_message
+
+        raise ImmediateHttpResponse(
+            HttpResponse(
+                render_to_string(
+                    "socialaccount/authentication_error.html",
+                    context=settings_context(_request=None),
+                )
+            )
+        )
+
     def pre_social_login(self, request, sociallogin):
         if sociallogin.is_existing == False:
             matching_person = Person.objects.filter(
                 social_provider_id=sociallogin.account.uid
             ).first()
 
-            if matching_person is None or matching_person.user_set.exists():
-                raise ImmediateHttpResponse(
-                    HttpResponse(
-                        render_to_string(
-                            "socialaccount/authentication_error.html",
-                            context=settings_context(_request=None),
-                        )
-                    )
+            if matching_person is None:
+                self._triggerStandardErrorPage(
+                    reasoning_message="There are no registered people in the application matching the values of the given social login."
                 )
-
-            sociallogin._person_pk = matching_person.pk
-        else:
-            pass  # User exists -- need to check what?
-
-        pass
+            sociallogin.existing_user = matching_person.user_set.get()
+            sociallogin.person_id = matching_person.pk
 
     def is_open_for_signup(self, request, sociallogin):
         return getattr(settings, "ALLOW_SSO", False)
@@ -70,14 +75,16 @@ class MicrosoftPersonAccountAdapter(DefaultSocialAccountAdapter):
         Override save_user to associate Person with User
         """
 
-        if not hasattr(sociallogin, "_person_pk"):
-            raise ImmediateHttpResponse(
-                HttpResponse(
-                    render_to_string(
-                        "socialaccount/authentication_error.html",
-                        context=settings_context(_request=None),
-                    )
-                )
+        existing_user = getattr(sociallogin, "existing_user", None)
+        if existing_user is not None:
+            sociallogin.user = existing_user
+            # Connect = True specifies that we want to connect the socialaccount to an existing user
+            sociallogin.save(request, connect=True)
+            return existing_user
+
+        if not hasattr(sociallogin, "person_id"):
+            self._triggerStandardErrorPage(
+                reasoning_message="Social login entity does not have a reference to the person entity."
             )
 
         user = super().save_user(request, sociallogin, form)
@@ -87,9 +94,10 @@ class MicrosoftPersonAccountAdapter(DefaultSocialAccountAdapter):
             raise Exception("'readonly' group does not exist")
         read_only_group.user_set.add(user)
 
-        user.person = Person.objects.get(id=sociallogin._person_pk)
+        user.person = sociallogin.person_id
         user.save()
 
-        delattr(sociallogin, "_person_pk")
+        delattr(sociallogin, "person_id")
+        delattr(sociallogin, "existing_user")
 
         return user
