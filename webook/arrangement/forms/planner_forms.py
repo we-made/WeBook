bc75b129357e7edd1fd3e6ab2c142245998b86e5
DESKTOP-T8VR2O2\Magnus Harlem <magnus@wemade.no>
2022-11-21 15:25:04 +0100

    fix bug where main planner could not be selected if there was no previous main planner

--- webook/arrangement/forms/planner_forms.py
+++ webook/arrangement/forms/planner_forms.py
@@ -3,7 +3,7 @@ from django import forms
 from webook.arrangement.models import Arrangement, Person
 
 
-class AddPlannersForm (forms.Form):
+class AddPlannersForm(forms.Form):
     arrangement_slug = forms.SlugField()
     planner_ids = forms.CharField()
 
@@ -11,21 +11,23 @@ class AddPlannersForm (forms.Form):
         return True
 
     def save(self):
-        arrangement = Arrangement.objects.get(slug=self.cleaned_data["arrangement_slug"])
+        arrangement = Arrangement.objects.get(
+            slug=self.cleaned_data["arrangement_slug"]
+        )
         print(self.cleaned_data.items())
         planner_pks = self.cleaned_data["planner_ids"].split(",")
         for planner_pk in planner_pks:
             planner_person = Person.objects.get(id=planner_pk)
-            
+
             if self.is_person_eligible(planner_person) is False:
                 raise "Person attempted to be added to arrangement is ineligible"
 
             arrangement.planners.add(planner_person)
-        
+
         arrangement.save()
 
 
-class AddPlannerForm (forms.Form):
+class AddPlannerForm(forms.Form):
     arrangement_id = forms.IntegerField()
     planner_person_id = forms.IntegerField()
 
@@ -34,8 +36,8 @@ class AddPlannerForm (forms.Form):
 
     def save(self):
         arrangement = Arrangement.objects.get(id=self.cleaned_data["arrangement_id"])
-        planner_person = Person.objects.get(id=self.cleaned_data["planner_person_id"]) 
-        
+        planner_person = Person.objects.get(id=self.cleaned_data["planner_person_id"])
+
         if self.is_person_eligible(planner_person) is False:
             raise "Person attempted to be added to arrangement is ineligible"
 
@@ -43,46 +45,49 @@ class AddPlannerForm (forms.Form):
         arrangement.save()
 
 
-class RemovePlannerForm (forms.Form):
+class RemovePlannerForm(forms.Form):
     arrangement_id = forms.IntegerField()
     planner_person_id = forms.IntegerField()
 
     def save(self):
         arrangement = Arrangement.objects.get(id=self.cleaned_data["arrangement_id"])
-        planner_person = Person.objects.get(id=self.cleaned_data["planner_person_id"]) 
+        planner_person = Person.objects.get(id=self.cleaned_data["planner_person_id"])
         arrangement.planners.remove(planner_person)
         arrangement.save()
 
 
-class RemovePlannersForm (forms.Form):
+class RemovePlannersForm(forms.Form):
     arrangement_slug = forms.SlugField()
     planner_ids = forms.CharField()
 
     def save(self):
-        arrangement = Arrangement.objects.get(slug=self.cleaned_data["arrangement_slug"])
+        arrangement = Arrangement.objects.get(
+            slug=self.cleaned_data["arrangement_slug"]
+        )
         planner_pks = self.cleaned_data["planner_ids"].split(",")
 
         for planner_pk in planner_pks:
             planner_person = Person.objects.get(id=planner_pk)
             arrangement.planners.remove(planner_person)
-        
+
         arrangement.save()
 
 
-class PromotePlannerToMainForm (forms.Form):
+class PromotePlannerToMainForm(forms.Form):
     arrangement_id = forms.IntegerField()
     promotee = forms.IntegerField()
 
     def promote(self):
-        arrangement = Arrangement.objects.get(id=self.cleaned_data["arrangement_id"]) 
-        
+        arrangement = Arrangement.objects.get(id=self.cleaned_data["arrangement_id"])
+
         promotee_person = arrangement.planners.get(id=self.cleaned_data["promotee"])
 
         # remove the promotee from the "common" planners group
         arrangement.planners.remove(promotee_person)
 
-        # demote the old main to a normal "planner" status
-        arrangement.planners.add(arrangement.responsible)
+        if arrangement.responsible is not None:
+            # demote the old main to a normal "planner" status
+            arrangement.planners.add(arrangement.responsible)
 
         arrangement.responsible = promotee_person
         arrangement.save()
