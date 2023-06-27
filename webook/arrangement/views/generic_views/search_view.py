a546a7784fb199e0d3232e1da9942290800c26d7
DESKTOP-T8VR2O2\Magnus Harlem <magnus@wemade.no>
2023-06-27 12:46:19 +0200

    simplify SearchView handling of the searchtype parametet

--- webook/arrangement/views/generic_views/search_view.py
+++ webook/arrangement/views/generic_views/search_view.py
@@ -1,38 +1,37 @@
 import json
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import JsonResponse
-from django.urls import reverse
-from django.core import serializers
-from django.views.generic import (
-    DetailView,
-    RedirectView,
-    UpdateView,
-    ListView,
-    CreateView,
-)
 from enum import Enum
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core import serializers
+from django.http import JsonResponse
+from django.urls import reverse
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    ListView,
+    RedirectView,
+    UpdateView,
+)
 
-class SearchView (ListView):
 
+class SearchView(ListView):
     class SearchType(Enum):
         TermSearch = 1
         PkSearch = 2
 
     def post(self, request):
-        body_data = json.loads(request.body.decode('utf-8'))
-        search_type = body_data["search_type"] if "search_type" in body_data else None
-
-        if (search_type is None):
-            search_type = self.SearchType.TermSearch
-        else:
-            search_type = self.SearchType(search_type)
+        body_data = json.loads(request.body.decode("utf-8"))
+        search_type = (
+            self.SearchType(body_data["search_type"])
+            if "search_type" in body_data
+            else self.SearchType.TermSearch
+        )
 
         response = None
-        if (search_type == self.SearchType.TermSearch):
+        if search_type == self.SearchType.TermSearch:
             search_term = body_data["term"]
             response = serializers.serialize("json", self.search(search_term))
-        if (search_type == self.SearchType.PkSearch):
+        if search_type == self.SearchType.PkSearch:
             pks = body_data["pks"]
             response = serializers.serialize("json", self.pk_search(pks))
 
