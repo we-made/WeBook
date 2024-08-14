40190ace2aa0bb7d26c7c634f8ab2a23d1f8e018
Magnus <magnus@wemade.no>
2024-08-14 09:34:51 +0200

    implement onlinebooking views

--- webook/onlinebooking/views.py
+++ webook/onlinebooking/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import DetailView, CreateView, UpdateView
 
-# Create your views here.
+from webook.onlinebooking.models import CitySegment, County, OnlineBooking, School
+
+
+class DashboardView(View):
+    def get(self, request):
+        return render(request, "dashboard.html")
+
+
+class OnlineBookingDetailView(DetailView):
+    model = OnlineBooking
+    template_name = "booking_detail.html"
+    context_object_name = "booking"
+
+
+class CountyDetailView(DetailView):
+    model = County
+    template_name = "county_detail.html"
+    context_object_name = "county"
+
+
+class SchoolDetailView(DetailView):
+    model = School
+    template_name = "school_detail.html"
+    context_object_name = "school"
+
+
+class CitySegmentDetailView(DetailView):
+    model = CitySegment
+    template_name = "city_segment_detail.html"
+    context_object_name = "city_segment"
+
+
+class SchoolCreateView(CreateView):
+    fields = ["name", "county", "city_segment"]
+    model = School
+    template_name = "school_form.html"
+
+    success_url = reverse_lazy("onlinebooking:dashboard")
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ["name", "county", "city_segment"]
+    model = School
+    template_name = "school_form.html"
+
+    success_url = reverse_lazy("onlinebooking:dashboard")
