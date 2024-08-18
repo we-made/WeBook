ddeff88d0ee8d25cf9c12a686970fc7314ea5f64
Magnus <magnus@wemade.no>
2024-08-18 11:17:37 +0200

    implement onlinebooking frontend flask app

---
+++ onlinebooking_app/app.py
@@ -0,0 +1,75 @@
+# save this as app.py
+from flask import Flask, url_for, render_template, request
+from api_client import WeBookApiClient
+import httpx
+import config
+
+app = Flask(__name__)
+api_client = WeBookApiClient()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", app_title=config.APP_TITLE) + render_template(
+        "table_component.html"
+    )
+
+
+@app.route("/counties")
+def counties():
+    response: httpx.Response = api_client.get("/onlinebooking/county/list")
+    return response.json()
+
+
+@app.route("/schools/<county_id>")
+def schools(county_id: int):
+    limit = request.args.get("limit", 10)
+    page = request.args.get("page", 0)
+    search = request.args.get("search", "")
+    sort_by = request.args.get("sort_by", "name")
+
+    response: httpx.Response = api_client.get(
+        f"/onlinebooking/school/list",
+        params={
+            "county_id": county_id,
+            "limit": limit,
+            "page": page,
+            "fields_to_search": "name" if search else "",
+            "search": search if search else "",
+            "sort_by": sort_by,
+            "sort_order": "desc",
+        },
+    )
+    return response.json()
+
+
+@app.route("/city_segments/<county_id>")
+def city_segments(county_id: int):
+    response: httpx.Response = api_client.get(
+        "/onlinebooking/city_segment/list?county_id=" + str(county_id)
+    )
+    return response.json()
+
+
+@app.route("/audiences")
+def audiences():
+    response: httpx.Response = api_client.get("/arrangement/audience/list")
+    return response.json()
+
+
+@app.post("/online_booking")
+def online_booking():
+    data = request.json
+    if "school_id" not in data:
+        data["school_id"] = None
+
+    response: httpx.Response = api_client.post(
+        "/onlinebooking/online_booking/create", json=data
+    )
+    return response.json()
+
+
+@app.get("/online_booking_settings")
+def online_booking_settings():
+    response: httpx.Response = api_client.get("/onlinebooking/online_booking/settings")
+    return response.json()
