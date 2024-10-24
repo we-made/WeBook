75a1e13ab7ad3445f44a658f19cf40813008e721
Magnus <magnus@wemade.no>
2024-10-24 10:32:13 +0200

    deactivate sentry because annoying in local dev

--- config/settings/base.py
+++ config/settings/base.py
@@ -57,16 +57,16 @@ DATABASES = {
 }
 
 
-sentry_sdk.init(
-    dsn=env.str("SENTRY_DSN", default=None),
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for tracing.
-    traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
-)
+# sentry_sdk.init(
+#     dsn=env.str("SENTRY_DSN", default=None),
+#     # Set traces_sample_rate to 1.0 to capture 100%
+#     # of transactions for tracing.
+#     traces_sample_rate=1.0,
+#     # Set profiles_sample_rate to 1.0 to profile 100%
+#     # of sampled transactions.
+#     # We recommend adjusting this value in production.
+#     profiles_sample_rate=1.0,
+# )
 
 if env.str("DATABASE_HOST", default=None):
     DATABASES["default"]["HOST"] = env.str("DATABASE_HOST")
