ce66d574faa05b2060755e1d4a0f14ab49c9531c
Magnus <magnus@wemade.no>
2024-11-27 15:29:04 +0100

    fixes and tweaks to repeating event mapping

--- webook/graph_integration/routines/mapping/repeating_event_mapping.py
+++ webook/graph_integration/routines/mapping/repeating_event_mapping.py
@@ -1,12 +1,12 @@
 from typing import List
-from msgraph.generated.models.calendar import Event as GraphEvent
+from msgraph.generated.models.event import Event as GraphEvent
 from webook.arrangement.models import (
     Event as WebookEvent,
     PlanManifest as WebookSerieManifest,
 )
 from msgraph.generated.models.item_body import ItemBody
 from msgraph.generated.models.location import Location
-from msgraph.generated.models.recipient import Attendee
+from msgraph.generated.models.attendee import Attendee
 from msgraph.generated.models.email_address import EmailAddress
 from msgraph.generated.models.body_type import BodyType
 from msgraph.generated.models.date_time_time_zone import DateTimeTimeZone
@@ -22,35 +22,19 @@ from webook.graph_integration.routines.mapping.single_event_mapping import (
 )
 
 
-def _map_daily_every_x_day_pattern(manifest: WebookSerieManifest) -> RecurrencePattern:
-    return RecurrencePattern(
-        type=RecurrencePatternType.Daily,
-        interval=manifest.interval,
-    )
-
-
-def _map_every_weekday_pattern(manifest: WebookSerieManifest) -> RecurrencePattern:
-    return RecurrencePattern(
-        type=RecurrencePatternType.Weekly,
-        days_of_week=[
-            DayOfWeek.Monday,
-            DayOfWeek.Tuesday,
-            DayOfWeek.Wednesday,
-            DayOfWeek.Thursday,
-            DayOfWeek.Friday,
-        ],
-        first_day_of_week=DayOfWeek.Monday,
-    )
-
-
 def _arbitrator_mapper(manifest: WebookSerieManifest) -> WeekIndex:
-    return {
+    week_index: WeekIndex = {
         0: WeekIndex.First,
         1: WeekIndex.Second,
         2: WeekIndex.Third,
         3: WeekIndex.Fourth,
         4: WeekIndex.Last,
-    }[int(manifest.arbitrator)]
+    }.get(int(manifest.arbitrator), None)
+
+    if not week_index:
+        raise ValueError("Invalid arbitrator value")
+
+    return week_index
 
 
 def _weekday_mapper(manifest: WebookSerieManifest) -> List[DayOfWeek]:
@@ -69,10 +53,29 @@ def _weekday_mapper(manifest: WebookSerieManifest) -> List[DayOfWeek]:
     ]
 
 
-def _map_weekly_pattern(manifest: WebookSerieManifest) -> RecurrencePattern:
-    if not manifest.interval:
-        raise ValueError("Interval must be specified for this pattern (weekly)")
+def _map_daily_every_x_day_pattern(manifest: WebookSerieManifest) -> RecurrencePattern:
+    return RecurrencePattern(
+        type=RecurrencePatternType.Daily,
+        interval=manifest.interval,
+    )
 
+
+def _map_every_weekday_pattern(manifest: WebookSerieManifest) -> RecurrencePattern:
+    return RecurrencePattern(
+        type=RecurrencePatternType.Weekly,
+        interval=manifest.interval or 1,
+        days_of_week=[
+            DayOfWeek.Monday,
+            DayOfWeek.Tuesday,
+            DayOfWeek.Wednesday,
+            DayOfWeek.Thursday,
+            DayOfWeek.Friday,
+        ],
+        first_day_of_week=DayOfWeek.Monday,
+    )
+
+
+def _map_weekly_pattern(manifest: WebookSerieManifest) -> RecurrencePattern:
     if not any(
         [
             manifest.monday,
@@ -99,10 +102,6 @@ def _map_weekly_pattern(manifest: WebookSerieManifest) -> RecurrencePattern:
 def _map_every_x_day_every_y_month_pattern(
     manifest: WebookSerieManifest,
 ) -> RecurrencePattern:
-    if not manifest.interval:
-        raise ValueError(
-            "Interval must be specified for this pattern (every x day every y month)"
-        )
     if not manifest.day_of_month:
         raise ValueError(
             "Day of month must be specified for this pattern (every x day every y month)"
@@ -110,7 +109,7 @@ def _map_every_x_day_every_y_month_pattern(
 
     return RecurrencePattern(
         type=RecurrencePatternType.AbsoluteMonthly,
-        interval=manifest.interval,  # Y month
+        interval=manifest.interval or 1,  # Y month
         # The day of the month on which the event occurs.
         # Required if type is absoluteMonthly or absoluteYearly.
         day_of_month=manifest.day_of_month,
@@ -120,14 +119,6 @@ def _map_every_x_day_every_y_month_pattern(
 def _map_every_arbitrary_date_of_month_pattern(
     manifest: WebookSerieManifest,
 ) -> RecurrencePattern:
-    if not manifest.interval:
-        raise ValueError(
-            "Interval must be specified for this pattern (every arbitrary date of month)"
-        )
-    if not manifest.day_of_month:
-        raise ValueError(
-            "Day of month must be specified for this pattern (every arbitrary date of month)"
-        )
     if not manifest.arbitrator:
         raise ValueError(
             "Arbitrator must be specified for this pattern (every arbitrary date of month)"
@@ -149,7 +140,7 @@ def _map_every_arbitrary_date_of_month_pattern(
 
     return RecurrencePattern(
         type=RecurrencePatternType.RelativeMonthly,
-        interval=manifest.interval,  # Y month
+        interval=manifest.interval or 1,  # Y month
         # The day of the week on which the event occurs.
         # Required if type is relativeMonthly or relativeYearly.
         days_of_week=_weekday_mapper(manifest),
@@ -170,14 +161,10 @@ def _map_yearly_every_x_of_month_pattern(
         raise ValueError(
             "Day of month must be specified for this pattern (yearly every x of month)"
         )
-    if not manifest.interval:
-        raise ValueError(
-            "Interval must be specified for this pattern (yearly every x of month)"
-        )
 
     return RecurrencePattern(
         type=RecurrencePatternType.AbsoluteYearly,
-        interval=manifest.interval,  # y year
+        interval=manifest.interval or 1,  # y year
         day_of_month=manifest.day_of_month,
         month=manifest.month - 1 if manifest.month else 1,
     )
@@ -186,14 +173,6 @@ def _map_yearly_every_x_of_month_pattern(
 def _map_every_arbitrary_weekday_in_month_pattern(
     manifest: WebookSerieManifest,
 ) -> RecurrencePattern:
-    if not manifest.interval:
-        raise ValueError(
-            "Interval must be specified for this pattern (every arbitrary weekday in month)"
-        )
-    if not manifest.day_of_month:
-        raise ValueError(
-            "Day of month must be specified for this pattern (every arbitrary weekday in month)"
-        )
     if not manifest.arbitrator:
         raise ValueError(
             "Arbitrator must be specified for this pattern (every arbitrary weekday in month)"
@@ -215,7 +194,7 @@ def _map_every_arbitrary_weekday_in_month_pattern(
 
     return RecurrencePattern(
         type=RecurrencePatternType.RelativeYearly,
-        interval=manifest.interval,  # y year
+        interval=manifest.interval or 1,  # y year
         # The day of the week on which the event occurs.
         # Required if type is relativeMonthly or relativeYearly.
         days_of_week=_weekday_mapper(manifest),
@@ -240,12 +219,12 @@ def _map_recurrence_pattern(manifest: WebookSerieManifest) -> RecurrencePattern:
 def _map_stop_within_range(manifest: WebookSerieManifest) -> RecurrenceRange:
     return RecurrenceRange(
         type=RecurrenceRangeType.EndDate,
-        start_date_time=DateTimeTimeZone(
+        start_date=DateTimeTimeZone(
             date_time=manifest.start_date.isoformat(),
             time_zone="UTC",
         ),
-        end_date_time=DateTimeTimeZone(
-            date_time=manifest.end_date.isoformat(),
+        end_date=DateTimeTimeZone(
+            date_time=manifest.stop_within.isoformat(),
             time_zone="UTC",
         ),
     )
@@ -254,18 +233,18 @@ def _map_stop_within_range(manifest: WebookSerieManifest) -> RecurrenceRange:
 def _map_stop_after_x_occurrences(manifest: WebookSerieManifest) -> RecurrenceRange:
     return RecurrenceRange(
         type=RecurrenceRangeType.Numbered,
-        start_date_time=DateTimeTimeZone(
+        start_date=DateTimeTimeZone(
             date_time=manifest.start_date.isoformat(),
             time_zone="UTC",
         ),
-        number_of_occurrences=manifest.occurrences,
+        number_of_occurrences=manifest.stop_after_x_occurences,
     )
 
 
 def _map_no_stop_date_range(manifest: WebookSerieManifest) -> RecurrenceRange:
     return RecurrenceRange(
         type=RecurrenceRangeType.NoEnd,
-        start_date_time=DateTimeTimeZone(
+        start_date=DateTimeTimeZone(
             date_time=manifest.start_date.isoformat(),
             time_zone="UTC",
         ),
@@ -281,7 +260,9 @@ def _map_recurrence_range(manifest: WebookSerieManifest) -> RecurrenceRange:
 
 
 def map_serie_to_graph_event(serie: WebookSerieManifest) -> GraphEvent:
-    sample_event = serie.events.first()
+    sample_event = (
+        WebookSerieManifest.objects.last().eventserie_set.first().events.first()
+    )
     base = map_event_to_graph_event(sample_event)
     base.start = None
     base.end = None
