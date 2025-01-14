648420038023c2936c8a26c5cd04601b3776f671
Magnus <magnus@wemade.no>
2025-01-14 17:05:15 +0100

    formatting

--- celery_app/celery.py
+++ celery_app/celery.py
@@ -1,5 +1,4 @@
 import os
-
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
@@ -10,9 +9,11 @@ app.autodiscover_tasks(packages=["webook.celery_haystack"])
 app.conf.result_backend = "django-db"
 app.conf.timezone = "Europe/Oslo"
 
+
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     pass
+
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
