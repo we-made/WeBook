4d85955e5cfac5047e77efefb92da55e87418b2e
Magnus <magnus@wemade.no>
2025-01-27 15:56:25 +0100

    correct bug in manifest_describe messing up serie load in

--- webook/utils/manifest_describe.py
+++ webook/utils/manifest_describe.py
@@ -55,45 +55,71 @@ _weekday_names_nb_no = {
 
 def _days_to_str(manifest) -> str:
     """Take the active days on a manifest and convert them to a readable summary
-    
+
     So for example;
         monday, friday, saturday are selected
         the end result should then be: 'monday, friday and saturday'
 
     Args:
         manifest (PlanManifest): the PlanManifest from which to summarize days
-    
+
     Returns:
         str: a comma separated summary of the active days on the given manifest
     """
 
     # regex replaces the last , with and
-    return re.sub(r'(,)(?!.*\1)', ' and', ", ".join(map(lambda x: calendar.day_name[x], filter(lambda x: manifest.days[x] == True, manifest.days))))
+    return re.sub(
+        r"(,)(?!.*\1)",
+        " and",
+        ", ".join(
+            map(
+                lambda x: calendar.day_name[x],
+                filter(lambda x: manifest.days[x] == True, manifest.days),
+            )
+        ),
+    )
+
 
 def _days_to_str_nb_no(manifest) -> str:
     """Take the active days on a manifest and convert them to a readable summary
-    
+
     So for example;
         monday, friday, saturday are selected
         the end result should then be: 'mandag, fredag og lørdag'
 
     Args:
         manifest (PlanManifest): the PlanManifest from which to summarize days
-    
+
     Returns:
         str: a comma separated summary of the active days on the given manifest
     """
 
     # regex replaces the last , with and
-    return re.sub(r'(,)(?!.*\1)', ' og', ", ".join(map(lambda x: _weekday_names_nb_no[calendar.day_name[x]], filter(lambda x: manifest.days[x] == True, manifest.days))))
+    return re.sub(
+        r"(,)(?!.*\1)",
+        " og",
+        ", ".join(
+            map(
+                lambda x: _weekday_names_nb_no[calendar.day_name[x]],
+                filter(lambda x: manifest.days[x] == True, manifest.days),
+            )
+        ),
+    )
+
 
 describe_pattern = {
     "daily__every_x_day": lambda manifest: _(f"Daily every {manifest.interval} day"),
     "daily__every_weekday": lambda manifest: _(f"Every weekday"),
     "weekly__standard": lambda manifest: _(f"Weekly every {_days_to_str(manifest)}"),
-    "month__every_x_day_every_y_month": lambda manifest: _(f"The {manifest.day_of_month} every {manifest.interval} month"),
-    "month__every_arbitrary_date_of_month": lambda manifest: _(f"Every {_arbitrators[manifest.arbitrator]} {calendar.day_name[manifest.day_of_week]} every {manifest.interval} month"),
-    "yearly__every_x_of_month": lambda manifest: _(f"The {manifest.day_of_month} {calendar.month_name[manifest.month]} every {manifest.interval} year(s)"),
+    "month__every_x_day_every_y_month": lambda manifest: _(
+        f"The {manifest.day_of_month} every {manifest.interval} month"
+    ),
+    "month__every_arbitrary_date_of_month": lambda manifest: _(
+        f"Every {_arbitrators[manifest.arbitrator]} {calendar.day_name[manifest.day_of_week]} every {manifest.interval} month"
+    ),
+    "yearly__every_x_of_month": lambda manifest: _(
+        f"The {manifest.day_of_month} {calendar.month_name[manifest.month]} every {manifest.interval} year(s)"
+    ),
     "yearly__every_arbitrary_weekday_in_month": lambda manifest: f"The {_arbitrators[manifest.arbitrator]} {calendar.day_name[manifest.day_of_week]} in {calendar.month_name[manifest.month]} every {manifest.interval} year(s)",
 }
 
@@ -102,22 +128,32 @@ describe_pattern_nb_no = {
     "daily__every_weekday": lambda manifest: "Hver ukedag",
     "weekly__standard": lambda manifest: f"Ukentlig hver {_days_to_str_nb_no(manifest)}",
     "month__every_x_day_every_y_month": lambda manifest: f"Den {manifest.day_of_month}. hver {manifest.interval}. måned",
-    "month__every_arbitrary_date_of_month": lambda manifest: f"Hver {_arbitrators_nb_no[manifest.arbitrator]} {_weekday_names_nb_no(calendar.day_name[manifest.day_of_week])} hver {manifest.interval}. måned",
-    "yearly__every_x_of_month": lambda manifest: f"Den {manifest.day_of_month}. {_month_names_nb_no(calendar.month_name[manifest.month])} hver {manifest.interval}. år",
-    "yearly__every_arbitrary_weekday_in_month": lambda manifest: f"Den {_arbitrators_nb_no[manifest.arbitrator]} {_weekday_names_nb_no(calendar.day_name[manifest.day_of_week])} i {_month_names_nb_no(calendar.month_name[manifest.month])} hver {manifest.interval}. år",
+    "month__every_arbitrary_date_of_month": lambda manifest: f"Hver {_arbitrators_nb_no[manifest.arbitrator]} {_weekday_names_nb_no[calendar.day_name[manifest.day_of_week]]} hver {manifest.interval}. måned",
+    "yearly__every_x_of_month": lambda manifest: f"Den {manifest.day_of_month}. {_month_names_nb_no[calendar.month_name[manifest.month]]} hver {manifest.interval}. år",
+    "yearly__every_arbitrary_weekday_in_month": lambda manifest: f"Den {_arbitrators_nb_no[manifest.arbitrator]} {_weekday_names_nb_no[calendar.day_name[manifest.day_of_week]]} i {_month_names_nb_no[calendar.month_name[manifest.month]]} hver {manifest.interval}. år",
 }
 
 """dict: lambda functions for reading the plan manifests pattern strategy and summarizing it"""
 
 
 describe_recurrence = {
-    "StopWithin": lambda manifest: _(f"between {manifest.start_date} and {manifest.stop_within}"),
-    "StopAfterXInstances": lambda manifest: _(f"after {manifest.stop_after_x_occurences} "),
-    "NoStopDate": lambda manifest: _(f"for eternity (project {manifest.project_x_months_into_future} months into the future)"),
+    "StopWithin": lambda manifest: _(
+        f"between {manifest.start_date} and {manifest.stop_within}"
+    ),
+    "StopAfterXInstances": lambda manifest: _(
+        f"after {manifest.stop_after_x_occurences} "
+    ),
+    "NoStopDate": lambda manifest: _(
+        f"for eternity (project {manifest.project_x_months_into_future} months into the future)"
+    ),
 }
 
+
 def _date_nb_no_format(date):
-    return f"{date.day}. {_month_names_nb_no[calendar.month_name[date.month]]} {date.year}"
+    return (
+        f"{date.day}. {_month_names_nb_no[calendar.month_name[date.month]]} {date.year}"
+    )
+
 
 describe_recurrence_nb_no = {
     "StopWithin": lambda manifest: f"mellom {_date_nb_no_format(manifest.start_date)} og {_date_nb_no_format(manifest.stop_within)}",
@@ -130,10 +166,10 @@ describe_recurrence_nb_no = {
 
 def describe_manifest(manifest) -> str:
     """Generate friendly description of manifest schedule
-    
+
     Args:
         manifest (PlanManifest): the PlanManifest for which to make a description
-    
+
     Returns:
         str: a description of the schedule of the given manifest
     """
@@ -142,5 +178,5 @@ def describe_manifest(manifest) -> str:
     if lang_code == "nb-no":
         # TODO: Work-around. Need to study how the localization works in more depth, doesn't trigger the correct language, even with correct code in request.
         return f"{describe_pattern_nb_no[manifest.pattern_strategy](manifest)} {describe_recurrence_nb_no[manifest.recurrence_strategy](manifest)}"
-    
+
     return f"{describe_pattern[manifest.pattern_strategy](manifest)} {describe_recurrence[manifest.recurrence_strategy](manifest)}"
