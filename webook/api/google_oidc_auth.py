83e857bc4fb07bbb2cba9fdbe4404e630b8bb072
Magnus <magnus@wemade.no>
2025-01-31 14:40:17 +0100

    fix token verification google oidc

--- webook/api/google_oidc_auth.py
+++ webook/api/google_oidc_auth.py
@@ -1,10 +1,11 @@
+import json
 import requests
 import jwt
 from webook.api.models import ServiceAccount
 from django.db.models import Q
 from ninja.security import HttpBearer
 from ninja.errors import HttpError
-
+from jwt.algorithms import RSAAlgorithm
 from webook import logger
 
 GOOGLE_ISSUER = "https://accounts.google.com"
@@ -13,8 +14,8 @@ GOOGLE_CERTS_URL = "https://www.googleapis.com/oauth2/v3/certs"
 
 def fetch_google_public_keys():
     response = requests.get(GOOGLE_CERTS_URL)
-    response.raise_for_status()
-    return response.json()
+    keys = response.json().get("keys", [])
+    return {key["kid"]: RSAAlgorithm.from_jwk(json.dumps(key)) for key in keys}
 
 
 def verify_google_jwt(token: str, audience: str):
@@ -26,22 +27,24 @@ def verify_google_jwt(token: str, audience: str):
 
         if key_id not in keys:
             raise Exception("Invalid key id")
-        
+
         decoded_token = jwt.decode(
-            token, 
+            token,
             keys[key_id],
             algorithms=["RS256"],
             audience=audience,
-            issuer=GOOGLE_ISSUER
+            issuer=GOOGLE_ISSUER,
+            verify=True,
         )
 
         email = decoded_token.get("email")
 
         if not email:
             raise Exception("Invalid email")
-    
+
         service_account = ServiceAccount.objects.filter(
-            Q(email=email) & Q(service_account_type=ServiceAccount.GOOGLE_SERVICE_ACCOUNT)
+            Q(email=email)
+            & Q(service_account_type=ServiceAccount.GOOGLE_SERVICE_ACCOUNT)
         ).first()
 
         if not service_account:
@@ -50,18 +53,19 @@ def verify_google_jwt(token: str, audience: str):
         return service_account
     except Exception as e:
         print("Token verification failed." + str(e))
-        logger.exception(e)
         return None
+
 
 class GoogleOidcBearer(HttpBearer):
     def authenticate(self, request, token):
         try:
             service_account = verify_google_jwt(token, "webook")
         except Exception as e:
-            logger.exception(e)
-            raise HttpError(403, "Unauthorized.")
-        
+            return None
+            # raise HttpError(403, "Unauthorized.")
+
         if not service_account:
-            raise HttpError(403, "Unauthorized.")
+            return None
+            # raise HttpError(403, "Unauthorized.")
 
         return service_account
